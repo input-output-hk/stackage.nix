7b1937cf13b6b9de5babb745ba21eef9fbf14d51
Moritz Angermann <moritz.angermann@gmail.com>
2018-07-23 14:07:58 +0800

    cleanup default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
-{ lts-9_1  = import ./package-set.nix { inherit pkgs; lts-def = import ./lts-9.1.nix; };
-  lts-11_2 = import ./package-set.nix { inherit pkgs; lts-def = import ./lts-11.2.nix; };
-  lts-11_5 = import ./package-set.nix { inherit pkgs; lts-def = import ./lts-11.5.nix; };
-  lts-12_0 = import ./package-set.nix { inherit pkgs; lts-def = import ./lts-12.0.nix; };  
+{ pkgs    ? import <nixpkgs> {}
+, hackage ? import <hackage>
+, haskell ? import <haskell> }@args:
+let mkPkgSet = path: import ./package-set.nix (args // { lts-def = import path; }); in
+{ lts-9_1  = mkPkgSet ./lts-9.1.nix;
+  lts-11_2 = mkPkgSet ./lts-11.2.nix;
+  lts-11_5 = mkPkgSet ./lts-11.5.nix;
+  lts-12_0 = mkPkgSet ./lts-12.0.nix;
+  lts-12_1 = mkPkgSet ./lts-12.1.nix;
+  lts-12_2 = mkPkgSet ./lts-12.2.nix;
+
   # nightlies
-  lts-2018-04-21 = import ./package-set.nix { inherit pkgs; lts-def = import ./nightly-2018-04-21.nix; };
-  lts-2018-05-02 = import ./package-set.nix { inherit pkgs; lts-def = import ./nightly-2018-05-02.nix; };
-  lts-2018-06-29 = import ./package-set.nix { inherit pkgs; lts-def = import ./nightly-2018-06-29.nix; };  
+  lts-2018-04-21 = mkPkgSet ./nightly-2018-04-21.nix;
+  lts-2018-05-02 = mkPkgSet ./nightly-2018-05-02.nix;
+  lts-2018-06-29 = mkPkgSet ./nightly-2018-06-29.nix;
 }
