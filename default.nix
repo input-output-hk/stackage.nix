d467124998c658c3cffed565825b7ddc07ea5e36
Moritz Angermann <moritz.angermann@gmail.com>
2018-10-09 17:33:57 +0800

    bump

--- default.nix
+++ default.nix
@@ -1,17 +1,9 @@
 { pkgs    ? import <nixpkgs> {}
 , hackage ? import <hackage>
 , haskell ? import <haskell> }@args:
-let mkPkgSet = path: import ./package-set.nix (args // { lts-def = import path; }); in
-{ lts-9_1  = mkPkgSet ./lts-9.1.nix;
-  lts-11_2 = mkPkgSet ./lts-11.2.nix;
-  lts-11_5 = mkPkgSet ./lts-11.5.nix;
-  lts-11_13= mkPkgSet ./lts-11.13.nix;
-  lts-12_0 = mkPkgSet ./lts-12.0.nix;
-  lts-12_1 = mkPkgSet ./lts-12.1.nix;
-  lts-12_2 = mkPkgSet ./lts-12.2.nix;
-
-  # nightlies
-  lts-2018-04-21 = mkPkgSet ./nightly-2018-04-21.nix;
-  lts-2018-05-02 = mkPkgSet ./nightly-2018-05-02.nix;
-  lts-2018-06-29 = mkPkgSet ./nightly-2018-06-29.nix;
-}
+let
+  mkPkgSet = path: import ./package-set.nix (args // { lts-def = import path; });
+  ltss = import ./ltss.nix mkPkgsSet;
+  nightlies = import ./nightlies.nix mkPkgSet;
+in
+ltss // nightlies
--- update.sh
+++ update.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
+# ensure we have the most recent submodules
 git submodule foreach git pull origin master
-for lts in {lts-haskell,stackage-nightly}/*.yaml; do $(find $NIX_TOOLS -type f -name "lts-to-nix") $lts > $(basename ${lts%.yaml}.nix); done
+# update lts/nightly descriptions.
+
+# update them all in parallel...
+N=$(getconf _NPROCESSORS_ONLN)
+for lts in {lts-haskell,stackage-nightly}/*.yaml
+do
+    $(find $NIX_TOOLS -type f -name "lts-to-nix") $lts > $(basename ${lts%.yaml}.nix) &
+    while [[ $(jobs -r -p | wc -l) -gt $N ]]; do
+	# can't use `wait -n` on older bash versions.
+	# e.g. what ships with macOS High Sierra
+	sleep 1;
+    done
+done
+wait
+# update nightlies
+echo "mkPkgSet: {" > nightlies.nix;
+for a in nightly-*.nix; do echo "  \"${a%%.nix}\" = mkPkgSet ./$a;" >> nightlies.nix; done;
+echo "}" >> nightlies.nix
+# update lts
+echo "mkPkgsSet: {" > ltss.nix;
+for a in lts-*.nix; do echo "  \"${a%%.nix}\" = mkPkgSet ./$a;" >> ltss.nix; done;
+echo "}" >> ltss.nix
