a0a9578aff77a427aab6fde406fd5c4d0aa9c0f1
IOHK <devops+nix-tools@iohk.io>
2019-05-30 01:32:28 +0000

    Automatic update for Thu May 30 01:32:27 UTC 2019

--- nightlies.nix
+++ nightlies.nix
@@ -1256,4 +1256,5 @@
   "nightly-2019-05-27" = import ./nightly-2019-05-27.nix;
   "nightly-2019-05-28" = import ./nightly-2019-05-28.nix;
   "nightly-2019-05-29" = import ./nightly-2019-05-29.nix;
+  "nightly-2019-05-30" = import ./nightly-2019-05-30.nix;
 }
