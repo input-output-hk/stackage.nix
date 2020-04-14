fb53cfd8cfe10d19d56ed9c11793441f15334a3b
IOHK <devops+nix-tools@iohk.io>
2020-04-14 01:02:05 +0000

    Automatic update for Tue Apr 14 01:02:05 UTC 2020

--- nightlies.nix
+++ nightlies.nix
@@ -1491,4 +1491,5 @@
   "nightly-2020-04-10" = import ./nightly-2020-04-10.nix;
   "nightly-2020-04-11" = import ./nightly-2020-04-11.nix;
   "nightly-2020-04-12" = import ./nightly-2020-04-12.nix;
+  "nightly-2020-04-13" = import ./nightly-2020-04-13.nix;
 }
