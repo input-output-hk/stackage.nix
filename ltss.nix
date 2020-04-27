0b43279fb9edd47aa005760ec2cd95b41d33faa5
IOHK <devops+nix-tools@iohk.io>
2020-04-27 01:02:36 +0000

    Automatic update for Mon Apr 27 01:02:36 UTC 2020

--- ltss.nix
+++ ltss.nix
@@ -327,4 +327,5 @@
   "lts-15.7" = import ./lts-15.7.nix;
   "lts-15.8" = import ./lts-15.8.nix;
   "lts-15.9" = import ./lts-15.9.nix;
+  "lts-15.10" = import ./lts-15.10.nix;
 }
--- nightlies.nix
+++ nightlies.nix
@@ -1501,4 +1501,6 @@
   "nightly-2020-04-23" = import ./nightly-2020-04-23.nix;
   "nightly-2020-04-24" = import ./nightly-2020-04-24.nix;
   "nightly-2020-04-25" = import ./nightly-2020-04-25.nix;
+  "nightly-2020-04-26" = import ./nightly-2020-04-26.nix;
+  "nightly-2020-04-27" = import ./nightly-2020-04-27.nix;
 }
