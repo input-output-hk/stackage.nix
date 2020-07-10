c0575c3c80114cbd506b7987dc04e342fdefd252
IOHK <devops+nix-tools@iohk.io>
2020-07-10 01:03:16 +0000

    Automatic update for Fri Jul 10 01:03:16 UTC 2020

--- nightlies.nix
+++ nightlies.nix
@@ -1555,4 +1555,6 @@
   "nightly-2020-07-06" = import ./nightly-2020-07-06.nix;
   "nightly-2020-07-07" = import ./nightly-2020-07-07.nix;
   "nightly-2020-07-08" = import ./nightly-2020-07-08.nix;
+  "nightly-2020-07-09" = import ./nightly-2020-07-09.nix;
+  "nightly-2020-07-10" = import ./nightly-2020-07-10.nix;
 }
