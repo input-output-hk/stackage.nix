ac43268924ac77f2e1b841630b6bc607850082e6
IOHK <devops+nix-tools@iohk.io>
2020-11-24 01:06:34 +0000

    Automatic update for Tue Nov 24 01:06:34 UTC 2020

--- nightlies.nix
+++ nightlies.nix
@@ -1668,4 +1668,5 @@
   "nightly-2020-11-20" = import ./nightly-2020-11-20.nix;
   "nightly-2020-11-21" = import ./nightly-2020-11-21.nix;
   "nightly-2020-11-22" = import ./nightly-2020-11-22.nix;
+  "nightly-2020-11-23" = import ./nightly-2020-11-23.nix;
 }
