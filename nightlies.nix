4981103cefb5c5d78a7b2567f79b8a4097aa7b65
IOHK <devops+nix-tools@iohk.io>
2021-01-05 01:04:04 +0000

    Automatic update for Tue Jan  5 01:04:03 UTC 2021

--- nightlies.nix
+++ nightlies.nix
@@ -1687,4 +1687,5 @@
   "nightly-2021-01-01" = import ./nightly-2021-01-01.nix;
   "nightly-2021-01-02" = import ./nightly-2021-01-02.nix;
   "nightly-2021-01-03" = import ./nightly-2021-01-03.nix;
+  "nightly-2021-01-04" = import ./nightly-2021-01-04.nix;
 }
