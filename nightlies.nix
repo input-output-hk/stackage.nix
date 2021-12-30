bdd239c39800fce58fe29884fcea1731097f1ff3
IOHK <devops+nix-tools@iohk.io>
2021-12-30 01:14:02 +0000

    Automatic update for Thu Dec 30 01:14:02 AM UTC 2021

--- nightlies.nix
+++ nightlies.nix
@@ -1897,4 +1897,6 @@
   "nightly-2021-12-24" = import ./nightly-2021-12-24.nix;
   "nightly-2021-12-25" = import ./nightly-2021-12-25.nix;
   "nightly-2021-12-26" = import ./nightly-2021-12-26.nix;
+  "nightly-2021-12-29" = import ./nightly-2021-12-29.nix;
+  "nightly-2021-12-30" = import ./nightly-2021-12-30.nix;
 }
