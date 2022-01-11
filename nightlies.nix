308844000fafade0754e8c6641d0277768050413
IOHK <devops+nix-tools@iohk.io>
2022-01-11 01:20:28 +0000

    Automatic update for Tue Jan 11 01:20:28 AM UTC 2022

--- nightlies.nix
+++ nightlies.nix
@@ -1904,4 +1904,5 @@
   "nightly-2022-01-04" = import ./nightly-2022-01-04.nix;
   "nightly-2022-01-05" = import ./nightly-2022-01-05.nix;
   "nightly-2022-01-06" = import ./nightly-2022-01-06.nix;
+  "nightly-2022-01-10" = import ./nightly-2022-01-10.nix;
 }
