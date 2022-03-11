02b9e7ea7304027b5d473233c2465d04a21a17e3
IOHK <devops+nix-tools@iohk.io>
2022-03-11 01:17:31 +0000

    Automatic update for Fri Mar 11 01:17:31 AM UTC 2022

--- nightlies.nix
+++ nightlies.nix
@@ -1936,4 +1936,5 @@
   "nightly-2022-03-02" = import ./nightly-2022-03-02.nix;
   "nightly-2022-03-03" = import ./nightly-2022-03-03.nix;
   "nightly-2022-03-09" = import ./nightly-2022-03-09.nix;
+  "nightly-2022-03-10" = import ./nightly-2022-03-10.nix;
 }
