3e945e88ffdf2d2251e56db002419627f32f17c9
IOHK <devops+nix-tools@iohk.io>
2022-06-15 01:15:31 +0000

    Automatic update for Wed Jun 15 01:15:31 AM UTC 2022

--- nightlies.nix
+++ nightlies.nix
@@ -2010,4 +2010,6 @@
   "nightly-2022-06-11" = import ./nightly-2022-06-11.nix;
   "nightly-2022-06-12" = import ./nightly-2022-06-12.nix;
   "nightly-2022-06-13" = import ./nightly-2022-06-13.nix;
+  "nightly-2022-06-14" = import ./nightly-2022-06-14.nix;
+  "nightly-2022-06-15" = import ./nightly-2022-06-15.nix;
 }
