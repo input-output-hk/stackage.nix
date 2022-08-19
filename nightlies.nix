2972c2af9e529f055b42714869329b0cfc839619
IOHK <devops+nix-tools@iohk.io>
2022-08-19 01:21:54 +0000

    Automatic update for Fri Aug 19 01:21:54 AM UTC 2022

--- nightlies.nix
+++ nightlies.nix
@@ -2059,4 +2059,5 @@
   "nightly-2022-08-16" = import ./nightly-2022-08-16.nix;
   "nightly-2022-08-17" = import ./nightly-2022-08-17.nix;
   "nightly-2022-08-18" = import ./nightly-2022-08-18.nix;
+  "nightly-2022-08-19" = import ./nightly-2022-08-19.nix;
 }
