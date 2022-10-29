73555ded402e3155ae0a082a2220a65df4115652
IOHK <devops+nix-tools@iohk.io>
2022-10-29 01:13:07 +0000

    Automatic update for Sat Oct 29 01:13:07 AM UTC 2022

--- nightlies.nix
+++ nightlies.nix
@@ -2106,4 +2106,6 @@
   "nightly-2022-10-25" = import ./nightly-2022-10-25.nix;
   "nightly-2022-10-26" = import ./nightly-2022-10-26.nix;
   "nightly-2022-10-27" = import ./nightly-2022-10-27.nix;
+  "nightly-2022-10-28" = import ./nightly-2022-10-28.nix;
+  "nightly-2022-10-29" = import ./nightly-2022-10-29.nix;
 }
