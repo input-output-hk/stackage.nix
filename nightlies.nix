2922bf87f81db4a69c42d0976fad11180568266c
IOHK <devops+nix-tools@iohk.io>
2023-11-23 00:09:23 +0000

    Automatic Update

--- nightlies.nix
+++ nightlies.nix
@@ -2397,4 +2397,5 @@
   "nightly-2023-11-19" = import ./nightly-2023-11-19.nix;
   "nightly-2023-11-20" = import ./nightly-2023-11-20.nix;
   "nightly-2023-11-21" = import ./nightly-2023-11-21.nix;
+  "nightly-2023-11-22" = import ./nightly-2023-11-22.nix;
 }
