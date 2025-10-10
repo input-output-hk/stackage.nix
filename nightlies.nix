2206a91df8d511d5c4a2f2bdf53c44ca67c19643
IOHK <devops+nix-tools@iohk.io>
2025-10-10 00:12:30 +0000

    Automatic Update

--- nightlies.nix
+++ nightlies.nix
@@ -2943,4 +2943,5 @@
   "nightly-2025-10-06" = import ./nightly-2025-10-06.nix;
   "nightly-2025-10-07" = import ./nightly-2025-10-07.nix;
   "nightly-2025-10-08" = import ./nightly-2025-10-08.nix;
+  "nightly-2025-10-09" = import ./nightly-2025-10-09.nix;
 }
