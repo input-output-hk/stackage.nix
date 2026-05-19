25064bf96a9fbae32f791d8e455a1a4113e7e378
IOHK <devops+nix-tools@iohk.io>
2026-05-19 00:37:11 +0000

    Automatic Update

--- nightlies.nix
+++ nightlies.nix
@@ -3130,4 +3130,5 @@
   "nightly-2026-05-15" = import ./nightly-2026-05-15.nix;
   "nightly-2026-05-16" = import ./nightly-2026-05-16.nix;
   "nightly-2026-05-17" = import ./nightly-2026-05-17.nix;
+  "nightly-2026-05-18" = import ./nightly-2026-05-18.nix;
 }
