7470f68340a3cf68913a05502afb5daedd1b84b1
IOHK <devops+nix-tools@iohk.io>
2026-08-05 00:28:29 +0000

    Automatic Update

--- ltss.nix
+++ ltss.nix
@@ -625,4 +625,5 @@
   "lts-24.50" = import ./lts-24.50.nix;
   "lts-24.51" = import ./lts-24.51.nix;
   "lts-24.52" = import ./lts-24.52.nix;
+  "lts-24.53" = import ./lts-24.53.nix;
 }
--- nightlies.nix
+++ nightlies.nix
@@ -3197,4 +3197,5 @@
   "nightly-2026-08-01" = import ./nightly-2026-08-01.nix;
   "nightly-2026-08-02" = import ./nightly-2026-08-02.nix;
   "nightly-2026-08-03" = import ./nightly-2026-08-03.nix;
+  "nightly-2026-08-04" = import ./nightly-2026-08-04.nix;
 }
